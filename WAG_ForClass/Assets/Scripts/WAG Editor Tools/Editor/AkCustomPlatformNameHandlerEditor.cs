dad9376726e55a9e8bf1e1bf9952a462e3075088
JJ Margolis <jdmargo1@uci.edu>
2022-01-30 15:17:40 -0800

    Added Unity & Wwise
    Initiated git lfs
    tracking:
    .bnk
    WwiseUnityIntegration.zip
    WwiseUnityIntegration_iOS.zip
    WwiseUnityIntegration_iOS_Src.zip
    libAkSoundEngine.a
    libAkMusicEngine.a

---
+++ WAG_ForClass/Assets/Scripts/WAG Editor Tools/Editor/AkCustomPlatformNameHandlerEditor.cs
@@ -0,0 +1,11 @@
+﻿public partial class AkBuildPreprocessor : UnityEditor.Build.IPreprocessBuildWithReport, UnityEditor.Build.IPostprocessBuildWithReport
+{
+	static partial void GetCustomPlatformName(ref string platformName, UnityEditor.BuildTarget target)
+	{
+		if(target == UnityEditor.BuildTarget.Android)
+		{
+			platformName = "Android_High";
+		}
+	}
+
+}
